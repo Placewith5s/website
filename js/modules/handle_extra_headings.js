eb302c5401fd9f64ef6e0f40987df754360dabc4
placewith5s <qsenkaisu@gmail.com>
2026-03-02 19:38:45 +0100

    Heading id improvements
    Remove h1 ID on faq page
    Improve h2 ID for contact form
    Auto add IDs for extra headings inside sections

---
+++ js/modules/handle_extra_headings.js
@@ -0,0 +1,3 @@
+import { all_headings_nav_friendly } from "https://cdn.jsdelivr.net/npm/doc-access@1.0.2/dist/doc-access.js";
+all_headings_nav_friendly(document.body
+    .querySelector('main'));
---
+++ ts/modules/handle_extra_headings.ts
@@ -0,0 +1,4 @@
+import { all_headings_nav_friendly } from "https://cdn.jsdelivr.net/npm/doc-access@1.0.2/dist/doc-access.d.ts";
+
+all_headings_nav_friendly(document.body
+    .querySelector('main') as HTMLElement);
